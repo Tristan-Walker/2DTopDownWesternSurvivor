e894d102b218274dc3a345870c4e7d03bdd5c926
Charanpreet0112 <c.gharyal@gmail.com>
2026-02-23 16:14:48 -0500

    Changed movment to wasd and changed player sprite

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -4,7 +4,7 @@ const SPEED = 5.0
 
 func _physics_process(delta: float) -> void:
 	
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	var direction := Vector3(input_dir.x, 0, input_dir.y)
 	
