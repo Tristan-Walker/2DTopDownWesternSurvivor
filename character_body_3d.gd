41e92dceaac40a1ad2e6dd41b225e98368b5c20f
Charanpreet0112 <c.gharyal@gmail.com>
2026-02-25 12:55:37 -0500

    Modified shooting code to shoot with arrow keys and added abillity to hold to fire

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 @export var bullet_scene: PackedScene
+var fire_rate := 0.3
+var fire_timer := 0.0
+var last_shoot_dir: Vector3 = Vector3.RIGHT   # Default direction
 
 const SPEED = 5.0
 
@@ -21,27 +24,30 @@ func _physics_process(_delta: float) -> void:
 	
 	move_and_slide()
 	
-	if Input.is_action_just_pressed("ui_accept"):
-		shoot()
+	#Shooting 
+	fire_timer -= _delta
+	var shoot_dir = Vector3.ZERO
+	
+	if Input.is_action_pressed("shoot_right"):
+		shoot_dir = Vector3.RIGHT
+	elif Input.is_action_pressed("shoot_left"):
+		shoot_dir = Vector3.LEFT
+	elif Input.is_action_pressed("shoot_up"):
+		shoot_dir = Vector3(0, 0, -1)
+	elif Input.is_action_pressed("shoot_down"):
+		shoot_dir = Vector3(0, 0, 1)
 
-func shoot():
-	
-	# Height at which to spawn the bullet
-	var spawn_height = Vector3(0, 0.5, 0) 
-	
-	# Initialize bullet & give it a position
+	if shoot_dir != Vector3.ZERO:
+		last_shoot_dir = shoot_dir
+
+	if shoot_dir != Vector3.ZERO and fire_timer <= 0:
+		shoot(last_shoot_dir)
+		fire_timer = fire_rate
+
+func shoot(dir: Vector3) -> void:
+
 	var bullet = bullet_scene.instantiate()
 	get_tree().root.add_child(bullet)
-	bullet.global_position = global_position + spawn_height
-	
-	# Get the movement input to decide bullet direction
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	# Default direction (if standing still, shoot the way the sprite faces)
-	var shoot_dir = Vector3.LEFT if $Sprite3D.flip_h else Vector3.RIGHT
-	
-	# If moving, shoot in the direction of movement
-	if input_dir.length() > 0:
-		shoot_dir = Vector3(input_dir.x, 0, input_dir.y).normalized()
-	bullet.direction = shoot_dir
-	
+
+	bullet.global_position = global_position + Vector3(0, 0.5, 0)
+	bullet.direction = dir
