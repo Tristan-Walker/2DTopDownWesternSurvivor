a875d6d47342fc61172b3850b475897aab8d7d9b
Charanpreet0112 <c.gharyal@gmail.com>
2026-03-01 16:07:23 -0500

    added ability to pause game and a pause menu pops up when paused

---
+++ stat_display.gd
@@ -0,0 +1,19 @@
+extends VBoxContainer
+
+@onready var coin_display = $CurrencyDisplay
+@onready var score_display = $ScoreDisplay
+
+var coins: int = 69
+var score: int = 420
+
+func _process(delta: float) -> void:
+	#coins = str(global.coins)
+	#score = str(global.current_score)
+	update_text()
+	
+func _ready():
+	update_text()
+	
+func update_text():
+	coin_display.text = "COINS: " + str(coins)
+	score_display.text = "SCORE: " + str(score)
