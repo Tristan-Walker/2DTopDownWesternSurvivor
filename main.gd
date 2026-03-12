741f894382b230dca5a9153622209155b8e7aae0
Charanpreet0112 <c.gharyal@gmail.com>
2026-03-12 00:33:56 -0400

    gave chest and inventory and items can be transfered between player inventory and chest inventory

--- main.gd
+++ main.gd
@@ -9,12 +9,20 @@ func _ready():
 	player.toggle_inventory.connect(toggle_inventory_interface)
 	inventory_interface.set_player_inventory_data(player.inventory_data)
 	
+	for node in get_tree().get_nodes_in_group("external_inventory"):
+		node.toggle_inventory.connect(toggle_inventory_interface)
+		
 	# Connect the signal from the bus to a local function
 	SignalBus.player_died.connect(_on_player_health_depleted)
 	%GameOverScreen.hide() # Ensure it's hidden at start
 
-func toggle_inventory_interface() -> void:
+func toggle_inventory_interface(external_inventory_owner = null) -> void:
 	inventory_interface.visible = !inventory_interface.visible
+	
+	if external_inventory_owner and inventory_interface.visible:
+		inventory_interface.set_external_inventory(external_inventory_owner)
+	else:
+		inventory_interface.clear_external_inventory()
 
 func _on_player_health_depleted() -> void:
 	%GameOverScreen.visible = true
