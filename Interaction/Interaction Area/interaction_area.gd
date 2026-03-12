8238fff47b472ff6672ea5cfa8da7d783b079692
Tristan-Walker <122651120+Tristan-Walker@users.noreply.github.com>
2026-03-11 20:57:13 -0400

    Interaction area bug fixes

--- Interaction/Interaction Area/interaction_area.gd
+++ Interaction/Interaction Area/interaction_area.gd
@@ -5,10 +5,6 @@ class_name InteractionArea
 
 var interact: Callable
 
-func _ready():
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
-
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		InteractionManager.register_area(self)
