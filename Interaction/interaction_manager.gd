366455f2bd138a2d12b24425beb0711f8d97ae3a
Charanpreet0112 <c.gharyal@gmail.com>
2026-03-11 20:30:27 -0400

    fixed issue where chest could be opened from anywhere

--- Interaction/interaction_manager.gd
+++ Interaction/interaction_manager.gd
@@ -19,7 +19,7 @@ func _process(delta):
 	if active_areas.size() > 0 and can_interact:
 		active_areas.sort_custom(_sort_by_distance_to_player)
 		label.text = base_text + active_areas[0].action_name
-		#label.global_position = active_areas[0].global_position
+		
 		var camera = get_viewport().get_camera_3d()
 		label.global_position = camera.unproject_position(active_areas[0].global_position)
 
