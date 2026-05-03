eb69d4179f2e6456e8526f20d64dfe397b83d172
Charanpreet0112 <c.gharyal@gmail.com>
2026-05-03 13:40:37 -0400

    added delay before being to make inputs when starting a level to prevent bullet uses imediatly, added working buttons to game over screen

--- UI/game_over_ui.gd
+++ UI/game_over_ui.gd
@@ -1,7 +1,5 @@
 extends CanvasLayer
 
-
-
 func _ready() -> void:
 	SignalBus.player_died.connect(_on_player_health_depleted)
 	self.hide()                          # Ensure it's hidden at start
@@ -10,3 +8,18 @@ func _on_player_health_depleted() -> void:
 	PlayerManager.is_game_over = true    # Tells PlayerManager that player is dead
 	self.show()                          # Show game over screen
 	get_tree().paused = true             # Pause game
+
+# Restart level
+func _on_restart_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+# Go back to Town or Main 
+func _on_home_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://main.tscn")
+
+# Go to Main Menu
+func _on_main_menu_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
