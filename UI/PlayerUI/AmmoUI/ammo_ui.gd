c7074ec55d466a7a030599cc995b76e0fb293644
Tristan-Walker <122651120+Tristan-Walker@users.noreply.github.com>
2026-06-10 03:05:17 -0400

    UI changes: layout, shows max ammo, rifle has different icons

--- UI/PlayerUI/AmmoUI/ammo_ui.gd
+++ UI/PlayerUI/AmmoUI/ammo_ui.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
-@onready var ammo_label = $MarginContainer/HBoxContainer/Label
+@onready var ammo_label = $MarginContainer/HBoxContainer/VBoxContainer/CurrentAmmo
+@onready var max_ammo_label = $MarginContainer/HBoxContainer/VBoxContainer/MaxAmmo
 @onready var bullet_grid = $MarginContainer/HBoxContainer/GridContainer
 
 var bullet_icons = []
@@ -14,7 +15,7 @@ func _ready():
 # Sets up initial ammo icons
 func _setup_icons(max_ammo: int, layout: AmmoLayout):
 	current_layout = layout   # Save current layout to be used outside this function
-	
+	max_ammo_label.text = str(max_ammo)
 	# Clear stale icon references FIRST
 	bullet_icons.clear()
 	
@@ -25,7 +26,6 @@ func _setup_icons(max_ammo: int, layout: AmmoLayout):
 	
 	# Use settings of the new layout
 	bullet_grid.columns = layout.grid_columns     # Use new layouts number of columns
-	var icon_size = layout.icon_size              # Use new layouts icon sizes
 	bullet_grid.add_theme_constant_override("h_separation", int(layout.icon_separation.x))
 	bullet_grid.add_theme_constant_override("v_separation", int(layout.icon_separation.y))
 	
@@ -33,7 +33,7 @@ func _setup_icons(max_ammo: int, layout: AmmoLayout):
 	for i in range(max_ammo):
 		var icon = layout.icon_scene.instantiate()
 		if icon is Control:
-			icon.custom_minimum_size = icon_size
+			icon.custom_minimum_size = layout.icon_size
 		bullet_grid.add_child(icon)
 		bullet_icons.append(icon)
 
